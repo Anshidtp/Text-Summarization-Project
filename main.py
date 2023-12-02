fa1ef0f8055f191d0fb720a8d249d939365d2343
Anshidtp <shaanshid07@gmail.com>
2023-12-02 21:13:46 +0530

    Data Transformation Notebook Added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textsummarizer.pipeline.stage01_data_ingestion import DataIngestionTrainPipeline
 from textsummarizer.pipeline.stage02_data_evaluation import DataValidationTrainPipeline
+from textsummarizer.pipeline.stage03_data_transformation import DataTransformationTrainPipeline
 from textsummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -17,6 +18,16 @@ try:
     logger.info(f"{STAGE_NAME} started")
     data_validation = DataValidationTrainPipeline()
     data_validation.main()
+    logger.info(f"{STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f"{STAGE_NAME} started")
+    data_transformation = DataTransformationTrainPipeline()
+    data_transformation.main()
     logger.info(f"{STAGE_NAME} completed")
 except Exception as e:
     logger.exception(e)
