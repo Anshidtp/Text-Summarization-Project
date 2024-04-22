7017f8faf4e74cda99ec8242b6c910ca592d051e
Anshidtp <shaanshid07@gmail.com>
2024-04-23 00:07:27 +0530

    model training notebook file added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textsummarizer.pipeline.stage01_data_ingestion import DataIngestionTrainPipeline
 from textsummarizer.pipeline.stage02_data_evaluation import DataValidationTrainPipeline
 from textsummarizer.pipeline.stage03_data_transformation import DataTransformationTrainPipeline
+from textsummarizer.pipeline.stage04_model_trainer import ModelTrainerTrainPipeline
 from textsummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -28,6 +29,16 @@ try:
     logger.info(f"{STAGE_NAME} started")
     data_transformation = DataTransformationTrainPipeline()
     data_transformation.main()
+    logger.info(f"{STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Training Stage"
+try:
+    logger.info(f"{STAGE_NAME} started")
+    model_trainer = ModelTrainerTrainPipeline()
+    model_trainer.main()
     logger.info(f"{STAGE_NAME} completed")
 except Exception as e:
     logger.exception(e)
