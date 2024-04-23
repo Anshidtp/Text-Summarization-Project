e7fb583d36e8c83a1e5ef3fb59f63ea7e84af495
Anshidtp <shaanshid07@gmail.com>
2024-04-23 23:29:49 +0530

    model training completed

--- src/textsummarizer/components/model_trainer.py
+++ src/textsummarizer/components/model_trainer.py
@@ -4,6 +4,7 @@ from transformers import DataCollatorForSeq2Seq
 from datasets import load_dataset , load_from_disk
 import torch
 from textsummarizer.entity import ModelTrainerConfig
+import os
 
 
 class ModelTrainer:
@@ -20,19 +21,27 @@ class ModelTrainer:
         #loading dataset
         dataset = load_from_disk(self.config.data_path)
 
+        # trainer_args = TrainingArguments(
+        #     output_dir= self.config.root_dir, 
+        #     num_train_epochs=self.config.num_train_epochs,
+        #     warmup_steps=self.config.warmup_steps,
+        #     per_device_train_batch_size=self.config.per_device_train_batch_size, 
+        #     per_device_eval_batch_size=self.config.per_device_eval_batch_size,
+        #     weight_decay= self.config.weight_decay, 
+        #     logging_steps=self.config.logging_steps,
+        #     evaluation_strategy=self.config.evaluation_strategy, 
+        #     eval_steps=self.config.eval_steps, 
+        #     save_steps=self.config.save_steps,
+        #     gradient_accumulation_steps=self.config.gradient_accumulation_steps
+        #     )
+
         trainer_args = TrainingArguments(
-            output_dir= self.config.root_dir, 
-            num_train_epochs=self.config.num_train_epochs,
-            warmup_steps=self.config.warmup_steps,
-            per_device_train_batch_size=self.config.per_device_train_batch_size, 
-            per_device_eval_batch_size=self.config.per_device_eval_batch_size,
-            weight_decay= self.config.weight_decay, 
-            logging_steps=self.config.logging_steps,
-            evaluation_strategy=self.config.evaluation_strategy, 
-            eval_steps=self.config.eval_steps, 
-            save_steps=self.config.save_steps,
-            gradient_accumulation_steps=self.config.gradient_accumulation_steps
-            )
+            output_dir=self.config.root_dir, num_train_epochs=1, warmup_steps=500,
+            per_device_train_batch_size=1, per_device_eval_batch_size=1,
+            weight_decay=0.01, logging_steps=10,
+            evaluation_strategy='steps', eval_steps=500, save_steps=1e6,
+            gradient_accumulation_steps=16
+        )
         
         trainer = Trainer(model=model_pegas, args=trainer_args,
                   tokenizer=tokenizer, data_collator=seq2seq_data_collator,
